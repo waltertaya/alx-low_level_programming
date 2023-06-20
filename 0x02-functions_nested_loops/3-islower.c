532a09d037e9fc2ab2db4cb1359bb635e0b37f74
waltertaya <waltermittybecker5@gmail.com>
2023-06-20 08:37:51 -0700

    int _islower(int c);

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ *int _islower(int c) - prints 1 if c is lowercase and 0 otherwise
+ */
+int _islower(int c)
+{
+  char letter = 'c';
+
+  if (letter = 'c')
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}

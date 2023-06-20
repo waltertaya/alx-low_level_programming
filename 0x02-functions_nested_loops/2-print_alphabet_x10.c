d1759f86f3192b2d92635f4612e97437f240919a
waltertaya <waltermittybecker5@gmail.com>
2023-06-20 08:24:27 -0700

    void print_alphabet_x10(void);

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,16 +7,16 @@
  */
 void print_alphabet_x10(void)
 {
-  char letter;
+ char letter;
   
-  int i;
+ int i;
   
-  for(i=1;i<11;i++)
-    {
-      for(letter='a';letter<='z';letter++)
-	{
-	  _putchar(letter);
-	}
-      _putchar('\n');
+ for (i = 1; i <= 10; i++)
+   {
+    for(letter='a';letter<='z';letter++)
+      {
+        _putchar(letter);
+       }
+     _putchar('\n');
     }
 }

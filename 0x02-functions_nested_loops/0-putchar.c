42634e84fb2d96b7e6fb7481acc49244f1872ec8
waltertaya <waltermittybecker5@gmail.com>
2023-06-20 06:50:03 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * main - prints '_putchar'
- * Return : always 0
+ * main - Print _putchar
+ * Return: Always 0
  */
-
 int main(void)
 {
-  _putchar('_');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');

50cf9b5a80e8c08d65333ba8f29eedfa6fad290f
waltertaya <waltermittybecker5@gmail.com>
2023-06-22 21:48:41 +0300

    updated

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,18 +12,18 @@
  */
 int main(void)
 {
-	int a;
+	int i;
 
-	for (a = 1; a <= 100; a++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (a % 15 == 0)
+		if (i % 15 == 0)
 			printf("FizzBuzz");
-		else if (a % 3 == 0)
+		else if (i % 3 == 0)
 			printf("Fizz");
-		else if (a % 5 == 0)
+		else if (i % 5 == 0)
 			printf("Buzz");
 		else
-			printf("%a", a);
+			printf("%i", i);
 		if (i < 100)
 			printf(" ");
 	}

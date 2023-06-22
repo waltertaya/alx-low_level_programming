fd04b365b5e4ea323b84be5dec909b7e8c60594b
waltertaya <waltermittybecker5@gmail.com>
2023-06-22 18:28:36 +0300

    updated

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,16 @@
 #include "main.h"
+
 /**
- * _isdigit - checks if the c is a digit
- * Returns: 0 or 1
- */
- 
+* _isdigit - checks if the c is a digit
+* Returns: 0 or 1
+**/
 int _isdigit(int c)
+
 {
 	if(c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
+
 }
+

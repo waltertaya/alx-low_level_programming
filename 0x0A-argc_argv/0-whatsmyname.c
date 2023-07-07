446c1ded28c4af185918464d143726c7da4a1195
waltertaya <waltermittybecker5@gmail.com>
2023-07-07 17:25:25 +0300

    updated

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
 /**
- * main - prints the name of the program
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0
+* main - prints the name of the program
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: 0
 */
-int main(int argc, char **argv[])
+int main(int argc, char *argv[])
 {
 if (argc == 1)
 {

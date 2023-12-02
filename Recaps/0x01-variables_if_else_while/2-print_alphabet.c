80cf5cb7950f4d00918ab9c76eb19bd9b9941cd6
waltertaya <waltermittybecker5@gmail.com>
2023-12-02 14:07:08 +0300

    recaps

---
+++ Recaps/0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i = 0;
+
+for (i = 97; i < 123; i++)
+{
+putchar(i);
+}
+putchar(10);
+
+return (0);
+}
